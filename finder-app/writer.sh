3a735aa5f75189a0d19e43a7adc76d6a3c0f47e3
ram066 <35061224+ram066@users.noreply.github.com>
2026-01-04 21:32:43 +0530

    Add writer.sh script for writing strings to files

---
+++ finder-app/writer.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+if [ $# -ne 2 ]; then
+ echo "Error:two argumentsrequired"
+ echo "usage:$0 <writefile> <writestr>"
+ fi
+ 
+ writefile="$1"
+ writestr="$2"
+
+writedir=$(dirname "$writefile")
+
+mkdir -p "$writedir"
+
+if [ $? -ne 0 ];then
+
+ echo "Error: failed to create directory path $writedir"
+ exit 1
+ 
+fi 
+
+echo "$writestr" > "$writefile"
+
+if [ $? -ne 0 ];then
+ echo "erroe:could not write to file:$writefile"
+ exit 1
+fi
+
+echo "successfully wrote to $writefile" 
+
+
+
