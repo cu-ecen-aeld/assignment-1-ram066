009f0af47c7fab4e9067b02e48f30ae088c23feb
ram066 <35061224+ram066@users.noreply.github.com>
2026-01-04 16:46:15 +0000

    Fix writer.sh and set executable permission

--- finder-app/writer.sh
+++ finder-app/writer.sh
@@ -1,32 +1,16 @@
-#!/bin/sh
+#!/bin/bash
+
 if [ $# -ne 2 ]; then
- echo "Error:two argumentsrequired"
- echo "usage:$0 <writefile> <writestr>"
- exit 1
- fi
- 
- writefile="$1"
- writestr="$2"
+    echo "Error: Two arguments required"
+    echo "Usage: $0 <writefile> <writestr>"
+    exit 1
+fi
+
+writefile="$1"
+writestr="$2"
 
 writedir=$(dirname "$writefile")
 
-mkdir -p "$writedir"
-
-if [ $? -ne 0 ];then
-
- echo "Error: failed to create directory path $writedir"
- exit 1
- 
-fi 
-
-echo "$writestr" > "$writefile"
-
-if [ $? -ne 0 ];then
- echo "erroe:could not write to file:$writefile"
- exit 1
-fi
-
-echo "successfully wrote to $writefile" 
-
-
+mkdir -p "$writedir" || exit 1
+echo "$writestr" > "$writefile" || exit 1
 
